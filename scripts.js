baeb017a8b5d913b185b038ab3a9f8c2b0a59360
theutkarshmahaliya <exorchrist.reek@gmail.com>
2024-06-29 15:43:30 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggleDetails(element) {
+    const details = element.querySelector('.details');
+    if (details.style.display === 'block') {
+        details.style.display = 'none';
+    } else {
+        details.style.display = 'block';
+    }
+}
